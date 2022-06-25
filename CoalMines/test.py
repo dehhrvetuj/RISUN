e7f06e054bba3739b8c3a4e6cbcfc926e0e7bcbe
yuehan_wang <34420665+dehhrvetuj@users.noreply.github.com>
2022-06-25 19:51:08 +0800

    Add files via upload

--- CoalMines/test.py
+++ CoalMines/test.py
@@ -1,22 +1,54 @@
 import mines_url
 import mines_detail
-import mines_detail
 import pandas as pd
 from labels import *
+import re
+
 
 data = pd.DataFrame(mines_detail.mines_global)
 # data = data.loc[400:405]
 
-
+data['Name'] = data['name']
+data['Country'] = data['country']
+data['Source'] = data['url']
 data['GPS'] = pd.concat([data[ind] for ind in gps_ind]).dropna().reindex_like(data)
 data['Status'] = pd.concat([data[ind] for ind in status_ind]).dropna().reindex_like(data)
-data['Coal_Type'] = pd.concat([data[ind] for ind in coal_type_ind]).dropna().reindex_like(data)
-data['Mine_Type'] = pd.concat([data[ind] for ind in mine_type_ind]).dropna().reindex_like(data)
+data['Coal Type'] = pd.concat([data[ind] for ind in coal_type_ind]).dropna().reindex_like(data)
+data['Mine Type'] = pd.concat([data[ind] for ind in mine_type_ind]).dropna().reindex_like(data)
+data['Mine Size'] = pd.concat([data[ind] for ind in mine_size_ind]).dropna().reindex_like(data)
+data['Mine Method'] = pd.concat([data[ind] for ind in mine_method_ind]).dropna().reindex_like(data)
+data['Start Year'] = pd.concat([data[ind] for ind in start_year_ind]).dropna().reindex_like(data)
+data['End Year'] = pd.concat([data[ind] for ind in end_year_ind]).dropna().reindex_like(data)
+data['Owner'] = pd.concat([data[ind] for ind in owner_ind]).dropna().reindex_like(data)
+data['Operator'] = pd.concat([data[ind] for ind in operator_ind]).dropna().reindex_like(data)
+data['Sponsor'] = pd.concat([data[ind] for ind in sponsor_ind]).dropna().reindex_like(data)
+data['Parent'] = pd.concat([data[ind] for ind in parent_ind]).dropna().reindex_like(data)
 
 
-print(data[['name','Status','Coal_Type','Mine_Type']])
+def GetLattitudeLongitude(coordinates, flag):
+    if not pd.isnull(coordinates):
+        coord = re.findall(r"\-?\d+\.?\d*", coordinates)
+    else:
+        return None
 
-# print(mines)
+    if len(coord) == 2:
+        return float(coord[0]) if flag == 'lattitude' else float(coord[1])
+    else:
+        return None
+
+data['Lattitude'] = data['GPS'].apply(lambda x: GetLattitudeLongitude(x,'lattitude'))
+data['Longitude'] = data['GPS'].apply(lambda x: GetLattitudeLongitude(x,'longitude'))
+
+
+
+output = data[['Name','Country','Source','Location','Lattitude','Longitude','Status','Coal Type','Mine Type','Mine Size',
+            'Mine Method','Start Year','End Year','Owner','Operator','Sponsor','Parent']]
+
+# print(output)
+
+output.to_excel('test.xlsx')
+
+
 
 
 
